782e4c53a6657ca4afcb4f9fd903ab912c0cbd36
umesh_arke <umesharke274@gmail.com>
2023-08-16 08:18:27 +0530

    updated codes 17 aug

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Student_Model(models.Model):
+
+    # id = models.AutoField() -- this field is automatically by django
+    name = models.CharField(max_length=150)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+    image = models.ImageField()
+    file = models.FileField()
+
+class Car_Model(models.Model):
+    car_name = models.CharField(max_length=500)
+    speed = models.IntegerField(default=50)
+    
+
