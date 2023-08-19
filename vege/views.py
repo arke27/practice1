6e148922bae8efbca774e2d2ca5e89774df5b650
arkeumesh <umesharke27>
2023-08-19 08:36:21 +0530

    update create and read task

--- vege/views.py
+++ vege/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
 
 # Create your views here.
 
 def receipes(request):
-    return render(request,'receipes.html')
+    if request.method == "POST":
+        
+        
+        data = request.POST
+        receipe_image = request.FILES.get('receipe_image')
+        receipe_name = data.get('receipe_name')
+        receipe_description = data.get('receipe_description')
+
+        Receipe.objects.create(
+            receipe_image = receipe_image,
+            receipe_name = receipe_name,
+            receipe_description = receipe_description,
+
+    )
+        return redirect('/receipes/')
+        
+    queryset = Receipe.objects.all()
+    context = {'receipes':queryset}
+    return render(request,'receipes.html',context)
