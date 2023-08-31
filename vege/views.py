a15413c508b5ca371cb40ba96c7ba572248571ec
arkeumesh <umesharke27>
2023-08-31 15:08:04 +0530

    8 aug 31

--- vege/views.py
+++ vege/views.py
@@ -152,13 +152,23 @@ def register(request):
 
     return render(request,'register.html')
 
+from django.db.models import Q   # import for to search by name or dept, it specially used for OR purpose
 
 def get_students(request):
     queryset = Student.objects.all()
 
+    if request.GET.get('search'):
+        search = request.GET.get('search')
+        queryset = queryset.filter(
+            Q(student_name__icontains = search) |
+            Q(department__department__icontains = search) |
+            Q(student_id__student_id__icontains = search)
+
+        )
+
     
 
-    paginator = Paginator(queryset, 5)  # Show 25 contacts per page.
+    paginator = Paginator(queryset, 5)  # Show 5 contacts per page.
 
     page_number = request.GET.get("page",1)
     page_obj = paginator.get_page(page_number)
